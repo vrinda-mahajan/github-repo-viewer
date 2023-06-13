11b728ffe7706540fcd328467b9bbe904cca9fb0
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-06-13 18:40:02 +0530

    implemented github headers cache

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,11 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:github_repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:github_repo_viewer/github/core/infrastructure/github_headers_cache.dart';
 import 'package:github_repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 
 class StarredReposRemoteService {
   final Dio _dio;
+  final GithubHeadersCache headersCache;
 
-  StarredReposRemoteService(this._dio);
+  StarredReposRemoteService(this._dio, this.headersCache);
   Future<RemoteResponse<List<GithubRepoDTO>>> getStarredReposPage(
       int page) async {}
 }
