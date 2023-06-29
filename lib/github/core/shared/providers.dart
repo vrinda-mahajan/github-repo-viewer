0b861d2084a4c1e0dfc55eb5e1f3ee1e1b32344b
vrinda-mahajan <vrindamahajan96@gmail.com>
2023-06-29 14:04:45 +0530

    added searched repos notifiers

--- lib/github/core/shared/providers.dart
+++ lib/github/core/shared/providers.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repo_viewer/core/shared/provider.dart';
 import 'package:github_repo_viewer/github/core/infrastructure/github_headers_cache.dart';
+import 'package:github_repo_viewer/github/repos/core/application/paginated_repos_notifier.dart';
+import 'package:github_repo_viewer/github/repos/searched_repos/application/searched_repos_notifier.dart';
+import 'package:github_repo_viewer/github/repos/searched_repos/infrastructure/searched_repos_remote_service.dart';
+import 'package:github_repo_viewer/github/repos/searched_repos/infrastructure/searched_repos_repository.dart';
 import 'package:github_repo_viewer/github/repos/starred_repos/application/starred_repo_notifier.dart';
 import 'package:github_repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_local_service.dart';
 import 'package:github_repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart';
@@ -12,7 +16,9 @@ final githubHeadersCacheProvider = Provider(
 
 final starredReposRemoteServiceProvider = Provider(
   (ref) => StarredReposRemoteService(
-      ref.watch(dioProvider), ref.watch(githubHeadersCacheProvider)),
+    ref.watch(dioProvider),
+    ref.watch(githubHeadersCacheProvider),
+  ),
 );
 
 final starredReposLocalServiceProvider = Provider(
@@ -27,6 +33,24 @@ final starredReposRepositoryProvider = Provider(
 );
 
 final starredReposNotifierProvider =
-    StateNotifierProvider<StarredReposNotifier, StarredRepoState>(
+    StateNotifierProvider<StarredReposNotifier, PaginatedRepoState>(
   (ref) => StarredReposNotifier(ref.watch(starredReposRepositoryProvider)),
+);
+
+final searchedReposRemoteServiceProvider = Provider(
+  (ref) => SearchedReposRemoteService(
+    ref.watch(dioProvider),
+    ref.watch(githubHeadersCacheProvider),
+  ),
+);
+
+final searchedReposRepositoryProvider = Provider(
+  (ref) => SearchedReposRepository(
+    ref.watch(searchedReposRemoteServiceProvider),
+  ),
+);
+
+final searchedReposNotifierProvider =
+    StateNotifierProvider<SearchedReposNotifier, PaginatedRepoState>(
+  (ref) => SearchedReposNotifier(ref.watch(searchedReposRepositoryProvider)),
 );
